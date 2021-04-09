6676f7fba340f0d083cf2b33f93252e241699273
Inês Figueiredo <inesgomesfigueiredo@gmail.com>
2021-04-09 12:40:12 +0100

    Improvements.

--- feed-script.js
+++ feed-script.js
@@ -1,24 +1,42 @@
 var destUrl = "http://netfreaks.herokuapp.com/home/feed/";
+
 $.get(destUrl, successCallback);
+
 function successCallback(response) {
+
     var usersTable = $("#users-list");
     var blob;
-    var row = $("<tr></tr>");
+    var row = $("<tr height='200px'></tr>");
     var cell;
     var count = 0;
-    response.forEach(function() {
-        count++;
-        for (let element in response) {
-            blob = "<div class=\"card\"><img src=\"" + element.picture + "\" class=\"card-img-top\"><div class=\"card-body\">" + 
-            "<h5 class=\"card-title\">" + element.name + "  " + element.age + "</h5><p class=\"card-text\"><b>" + element.kinks + "</b></p>"
-            "<p class=\"card-text\">" + 
-            element.aboutMe + "</p></div></div>";
-            cell = $("<td></td>").text(blob);
-            row.append(cell);
-        }
-        if (count % 2 == 0) {
-            usersTable.append(row);
-            row = $("<tr></tr>");
-        }
-    });
+
+for (var x = 1; x <= response.length; x++) {
+    console.log("HEY!");
+    /*var divCard=$("<div></div>");
+    var divCardBody=$("<div ></div>");
+    var h5=$("<h5 ></h5>");
+    h5.text("HELLO");
+    divCardBody.append(h5);
+    divCard.append(divCardBody);*/
+
+ blob = "<div class='card' height='150px'><div class='card-body'><img src='" + response[x].picture + "' class='card-img-top'><h5 class='card-title'>" 
+    + response[x].name + "  " + response[x].age + "</h5><p class='card-text'><b>" + response[x].kinks 
+    + "</b></p></div><button type='button' id='" + response[x].name + "' class='btn btn-primary'>See more ;)</button></div>";
+
+cell = $("<td width=\"50%\" height='200px'></td>").html(blob);
+row.append(cell);
+        
+if (x % 2 == 0) {
+    usersTable.append(row);
+    row = $("<tr height='200px'></tr>");
 }
+}
+$(".btn-primary").on("click", openPopUp);
+
+}
+function openPopUp(data) {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+
