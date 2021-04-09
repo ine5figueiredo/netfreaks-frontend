1070d347112478c8068e7ecfc63550882e13060c
Inês Figueiredo <inesgomesfigueiredo@gmail.com>
2021-04-09 13:34:53 +0100

    Cleaned the path.

--- feed-script.js
+++ feed-script.js
@@ -12,7 +12,7 @@ function successCallback(response) {
 for (var x = 1; x <= response.length; x++) {
 
  blob = "<div class='card' height='150px'><div class='card-body'><img src='" + response[x].picture + "' class='card-img-top'><h5 class='card-title'>" 
-    + response[x].name + "  " + response[x].age + "</h5><p class='card-text'><b>" + response[x].kinks + "</b><br>" + response[x].aboutMe + 
+    + response[x].name + "  " + response[x].age + "</h5><p class='card-text'><b>" + response[x].kinks + "</b><br>" + response[x].aboutMe +
     "</p><button type='button' class='btn btn-primary'>See more ;)</button></div></div>";
 
 
@@ -23,6 +23,11 @@ if (x % 2 == 0) {
     usersTable.append(row);
     row = $("<tr height='200px'></tr>");
 }
+}
+$(".btn-primary").on("click", openPopUp);
+function openPopUp() {
+    alert("You have a m@tch!!!!");
+    
 }
 }
 
